af756e93d4c887416cd09d0fd3a96b06bdf14fc3
Kool Havis <havisk@gmail.com>
2019-07-03 19:44:20 -0400

    added files for homepage

--- src/hompage.component.jsx
+++ src/hompage.component.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 
+import "./homepage.styles.scss";
+
 const HomePage = () => (
   <div className="homepage">
-    <div classname="directory-menu">
+    <div className="directory-menu">
       <div className="menu-item">
         <div className="content">
           <h1 className="title">HATS</h1>
-          <span className="subtitle">Shop Now </span>
+          <span className="subtitle">Shop Now</span>
         </div>
       </div>
       <div className="menu-item">
